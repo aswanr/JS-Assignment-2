32e70c6eb6d9860265567e34a8020d463c9051b7
Aswan R <aswan.r@coddletech.com>
2024-10-28 16:22:28 +0530

    first commit

---
+++ assignment-2.js
@@ -0,0 +1,54 @@
+// 1) ​Console this star pattern
+for (let i=1; i<=5; i++) {
+    let gap = "";
+    for (let j=i; j<5; j++) {
+        gap += " ";
+    }
+    for (let k=0; k<i; k++) {
+        gap += "* ";
+    }
+    console.log(gap);
+}
+
+//2)['car', 1, 5, 'js', 'node js', 'mysql', 'sql' ] : Use switch statement inside a map function and 
+// console each item by using switch statement.
+const items = ['car', 1, 5, 'js', 'node js', 'mysql', 'sql'];
+items.map(i => {
+    switch(typeof i){
+        case 'string':
+            console.log(`its srting ${i}`);
+            break;
+        case 'number':
+                console.log(`it is number ${i}`);
+                break;
+        case 'boolean':
+            console.log(`its is charater ${i}`);
+            break;
+        default:
+            console.log(`type unfounded iteam charater${i}`);
+    };
+})
+
+// 3)[1, 2, 3, 4.5, 6] : Write a closure function to delete indexes 5 and 2 of this array.
+function dele(){
+    let array= [1, 2, 3, 4.5, 6];
+    function delposition(){
+            array.splice(array.indexOf(2),1);
+            array.splice(array.indexOf(5),1);
+            console.log(array);                
+    }
+    return delposition;
+}
+let closure=dele();
+closure();
+
+//4)Use constructor method to console all elements of the passed array. (the array will be passed to the constructor method)
+const array12=[1,2,3,5]
+var aswan=new Function("array12","array12.forEach(i =>{console.log(i);})");
+aswan(array12);
+
+// 5)Create a self-invoking function to console you name.
+(function() {
+    console.log("Aswan.r"); 
+}());
+
